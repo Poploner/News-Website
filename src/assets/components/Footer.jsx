97c47e54e167aa4f659588f15eaa0cc7a01c57d5
nagudu <halifashuaibu12@gmail.com>
2025-10-23 11:33:53 +0100

    your comment

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer () {
             <div className="container mx-auto md:px-8 flex flex-col md:flex-row gap-[300px] items-center ">
                 <div>
                <div className="flex gap-[9px]">
-                 <p className="text-[15px] font-bold inline text-black bg-white w-[50px] h-[50px] rounded-[50%] flex justify-center items-center">Today</p>
+                 <p className="text-[15px] font-bold inline text-black bg-white w-[40px] h-[40px] rounded-[50%] flex justify-center items-center"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAABgUlEQVR4AexUMU7DQBAcwwMQDRUS8AIegBCIL9BR0VDRUPANJApKCh5ASUEHNEiUSDwAClokGgoKjlnHZ0Y2Xh+xcVJktXO3t7vZSS5jz2FCNiMe7OKn4arDBhDeiNADbA7nNV+g/uIdti0SfbjNsXmNs5R4vrFrvII7T4mr4x+ZuPsFL8xF17r1x3xlD9v8+wyrseARHwEZm6vABUrTGtiPJrthwbDPPXePOG/4r6UjcbjlFRbAKf5gHYmxRa6IdcbJ3pU4UVz179OROFlcNeaOxLV5yQmPmGJR8cQY5SPxIyyrgf1INo/YxBKFo/uKTNe89UvJDz1iexOpeNpi6/fZpOoR802k4mmLwX4km0ecPGScRo+YYjHRpALsh9rS6BCKPT+95ysXJf7kWd3EouJpi61fP79H1Z8xcUlEf4iBEj/FZE/7AuccEpuE+TWQ3aMwJb5i7px4Jfr0Zw47IXaJ0oU4+wKyA2KZyHrEGmcdEx8QE2LJDhDOiAe45BHFNwAAAP//hYZe+AAAAAZJREFUAwCmgY096ND2FwAAAABJRU5ErkJggg=="/></p>
                <span className="inline-block font-bold justify-center items-center gap-2 flex text-xl hover:underline"> NEWS</span> 
                </div>
                     <div className="flex-col pt-6 gap-2 leading-7 text-lg">
